da36a34be4b0a3f202410366951d62b880e1aad2
Selton <selton.bae@gmail.com>
2023-01-09 22:06:44 -0300

    comparando variaveis

---
+++ day1/main.js
@@ -0,0 +1,14 @@
+let numeroUm = 1
+let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta = '30'
+let numeroDez = 10
+let stringDez = '10'
+
+function compara(numero, string){
+    if(numero == string){
+        console.log(`As variaveis ${numero} e ${string} possuem o mesmo valor, mas tipos diferentes`)
+    }else{
+        console.log(`As variaveis ${numero} e ${string} não possuem o mesmo valor`)
+    }
+}
