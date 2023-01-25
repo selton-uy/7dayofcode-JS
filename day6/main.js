6ebf0786601a08db75b87956c43fca5739d7daad
Selton <selton.bae@gmail.com>
2023-01-24 22:39:26 -0300

    adicionando função de excluir

--- day6/main.js
+++ day6/main.js
@@ -1,10 +1,39 @@
 let produto = document.getElementById('produto');
 let categoria = document.getElementById('categoria');
-let carrinho = document.querySelector('div#carrinho');
+let carrinho = [];
+let produtosDoCarrinho = document.getElementById('produtosDoCarrinho')
+
+function mostraCarrinho(){
+    produtosDoCarrinho.innerHTML = carrinho;
+    console.log(carrinho)
+
+}
+function verificaCategoria(){
+    if(categoria.value == 0){
+        alert('Selecione uma caetgoria');
+    }
+}
 function enviar(){
+    verificaCategoria();
 
-    carrinho.innerHTML = produto.value;
+    if (produto.value == ''){
+        alert('Verifique as informações e tente novamente');
+        if(carrinho.length == 0){
+            excluir();
+        }
+    
+    }else{
+        carrinho.push(produto.value);
+        carrinho.push(categoria.value)
+        mostraCarrinho();
+    }
+    produto.value = ''
 
+}
+function excluir(){
+    carrinho.pop();
+    carrinho.pop();
+    mostraCarrinho();
 
 }
 
